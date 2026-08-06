146be734ed439a3f97f05968b420f2d82301b6be
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2026-08-06 11:17:46 -0300

    fix: update GroupBy conditional baseline for parenthesized ELSE and DIV
    EF Core 10 now wraps the ELSE branch of CASE expressions in parens
    and uses DIV for integer division.

--- test/EFCore.MySql.FunctionalTests/Query/NorthwindGroupByQueryMySqlTest.cs
+++ test/EFCore.MySql.FunctionalTests/Query/NorthwindGroupByQueryMySqlTest.cs
@@ -387,10 +387,10 @@ GROUP BY `o0`.`Name`, `o0`.`CustomerID`
 """
 SELECT `o`.`OrderDate` AS `Key`, CASE
     WHEN COUNT(*) = 0 THEN 1
-    ELSE COALESCE(SUM(CASE
+    ELSE (COALESCE(SUM(CASE
         WHEN (`o`.`OrderID` % 2) = 0 THEN 1
         ELSE 0
-    END), 0) / COUNT(*)
+    END), 0)) DIV (COUNT(*))
 END AS `SomeValue`
 FROM `Orders` AS `o`
 GROUP BY `o`.`OrderDate`
